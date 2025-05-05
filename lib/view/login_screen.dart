206d328c41da2b6419c914cdd84f7a498a9fd98d
Afaf Darai <afafdarai2006@gmail.com>
2025-05-05 17:09:46 -0600

    appTest

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -7,43 +7,36 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-
-
-
-class _LoginScreenState extends State<LoginScreen>
-{
+class _LoginScreenState extends State<LoginScreen> {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors:
-            [
-              Colors.pink,
-              Colors.amber,
-
+            colors: [
+              Colors.blue,
+              Colors.white,
             ],
-            begin:FractionalOffset(0, 0),
+            begin: FractionalOffset(0, 0),
             end: FractionalOffset(1, 0),
-            stops: [0,1],
+            stops: [0, 1],
             tileMode: TileMode.clamp,
           ),
         ),
-
         child: ListView(
           children: [
-
             Image.asset(
-                "images/logo.png"
+              "images/logo.png",
+              // Tu peux aussi définir la taille de l'image si nécessaire
+              height: 200.0,
+              width: 200.0,
             ),
-
             const Text(
               "Welcome to LocaHome",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                color: Colors.pink,
+                color: Colors.white,
                 fontSize: 30.0,
               ),
               textAlign: TextAlign.center,
--- lib/view/spalsh_screen.dart
+++ lib/view/spalsh_screen.dart
@@ -4,8 +4,6 @@ import 'package:get/get.dart';
 
 import 'login_screen.dart';
 
-
-
 class SpalshScreen extends StatefulWidget {
   const SpalshScreen({super.key});
 
@@ -13,62 +11,52 @@ class SpalshScreen extends StatefulWidget {
   State<SpalshScreen> createState() => _SpalshScreenState();
 }
 
-class _SpalshScreenState extends State<SpalshScreen>
-{
+class _SpalshScreenState extends State<SpalshScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
-    Timer(const Duration(seconds: 3),(){
+    Timer(const Duration(seconds: 3), () {
       Get.to(LoginScreen());
     });
   }
+
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors:
-              [
-                Colors.pink,
-                Colors.amber,
-
-              ],
-            begin:FractionalOffset(0, 0),
+            colors: [
+              Colors.blue, // Première couleur : bleu
+              Colors.white, // Deuxième couleur : jaune
+            ],
+            begin: FractionalOffset(0, 0),
             end: FractionalOffset(1, 0),
-            stops: [0,1],
+            stops: [0, 1],
             tileMode: TileMode.clamp,
           ),
         ),
-        child:Center(
+        child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "images/splash.png"
-                ),
-
-                const Padding(
-                  padding: const EdgeInsets.only(top:18),
-                  child: Text(
-                    "Welcome to LocaHome",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.white,
-                    ),
+            children: [
+              Image.asset("images/splash.png"),
+              const Padding(
+                padding: EdgeInsets.only(top: 18),
+                child: Text(
+                  "Welcome to LocaHome",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white,
                   ),
                 ),
-
-
-              ],
+              ),
+            ],
           ),
         ),
-
       ),
     );
   }
